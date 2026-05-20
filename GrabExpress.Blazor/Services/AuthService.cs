ecdf9264f2867b8e6980d912fa729df2f5aaa6c1
Gabriel Borces <your-email@example.com>
2026-05-21 00:11:41 +0800

    feat: adapt AuthService for web storage and auth state

--- GrabExpress.Blazor/Services/AuthService.cs
+++ GrabExpress.Blazor/Services/AuthService.cs
@@ -1,15 +1,21 @@
+using Blazored.LocalStorage;
 using Firebase.Auth;
 using Firebase.Auth.Providers;
-using Newtonsoft.Json;
+using Microsoft.AspNetCore.Components.Authorization;
 
 namespace GrabExpress.Blazor.Services
 {
     public class AuthService
     {
         private readonly FirebaseAuthClient _authClient;
+        private readonly ILocalStorageService _localStorage;
+        private readonly AuthenticationStateProvider _authStateProvider;
 
-        public AuthService()
+        public AuthService(ILocalStorageService localStorage, AuthenticationStateProvider authStateProvider)
         {
+            _localStorage = localStorage;
+            _authStateProvider = authStateProvider;
+
             var config = new FirebaseAuthConfig
             {
                 ApiKey = FirebaseConfig.ApiKey,
@@ -25,17 +31,36 @@ namespace GrabExpress.Blazor.Services
 
         public async Task<UserCredential> RegisterAsync(string email, string password)
         {
-            return await _authClient.CreateUserWithEmailAndPasswordAsync(email, password);
+            var creds = await _authClient.CreateUserWithEmailAndPasswordAsync(email, password);
+            await HandleLoginSuccess(creds);
+            return creds;
         }
 
         public async Task<UserCredential> LoginAsync(string email, string password)
         {
-            return await _authClient.SignInWithEmailAndPasswordAsync(email, password);
+            var creds = await _authClient.SignInWithEmailAndPasswordAsync(email, password);
+            await HandleLoginSuccess(creds);
+            return creds;
         }
 
-        public void Logout()
+        private async Task HandleLoginSuccess(UserCredential creds)
+        {
+            var token = await creds.User.GetIdTokenAsync();
+            await _localStorage.SetItemAsync("authToken", token);
+            await _localStorage.SetItemAsync("userEmail", creds.User.Info.Email);
+            await _localStorage.SetItemAsync("userUid", creds.User.Uid);
+
+            ((FirebaseAuthStateProvider)_authStateProvider).NotifyUserAuthentication(token, creds.User.Info.Email);
+        }
+
+        public async Task LogoutAsync()
         {
             _authClient.SignOut();
+            await _localStorage.RemoveItemAsync("authToken");
+            await _localStorage.RemoveItemAsync("userEmail");
+            await _localStorage.RemoveItemAsync("userUid");
+
+            ((FirebaseAuthStateProvider)_authStateProvider).NotifyUserLogout();
         }
 
         public User? GetCurrentUser()
